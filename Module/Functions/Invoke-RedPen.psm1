b9aeb5af1626c00d994613b58a279b36b88013a2
Brandon Lundt <brandon.lundt@plexus.com>
2018-04-02 14:56:40 -0500

    File reformat for ScriptAnalyzer recommendations

--- Module/Functions/Invoke-RedPen.psm1
+++ Module/Functions/Invoke-RedPen.psm1
@@ -1,5 +1,5 @@
 Function Invoke-RedPen {
-    <#
+	<#
 	.Synopsis
 	   Wrapper module for Powershell Module Pester, to assist in test promotion processes.
 	.DESCRIPTION
@@ -11,120 +11,118 @@ Function Invoke-RedPen {
 		Author: Brandon Lundt
 		Requires: PowerShell v5 and Pester 3.4.3 or newer		
 	#>
-    [cmdletbinding()]
-    param(
-        [parameter(Mandatory = $true)]
-        [ValidateNotNullOrEmpty()]
-        [ValidateScript( {
-                if ( $_ -like "*.json") {
-                    if ( Test-Path $_) {
-                        $True
-                    }#Test-Path True
-                    else {
-                        Throw "Unable to locate $_"
-                    }#Test-Path false
-                }#if match json true
-                else {
-                    Throw "$_ must be a valid psd1 or JSON file"
-                }#if match json false
-            }
-        )]
-        [string]$ConfigFile,
-        [parameter(
-            Mandatory = $false
-        )]
-        [ValidateSet("Simple", "Comprehensive")]
-        [string]$TestType = "Simple",
-        [parameter(Mandatory = $False)]
-        [ValidateNotNullOrEmpty()]
-        [string[]]$Tags,
-        [parameter(
-            Mandatory = $true,
-            ValueFromPipeline = $true
-        )]
-        [ValidateNotNullOrEmpty()]
-        [ValidateScript(
-            {    
-                Import-Module $_ 
-                if ( (Get-Module $_).Count -ge 1) {
-                    $true
-                }
-                else {
-                    throw "$_ is not a valid application module"
-                }
-            }
-        )]
-        [string]$Application,
-        [Parameter(Mandatory = $True)]
-        [string]$SmtpServer,
-        [Parameter(Mandatory = $True)]
-        [string]$From,
-        [parameter(Mandatory = $true)]
-        [ValidateNotNullOrEmpty()]
-        [string[]]$To
-    )
-    begin {
-        New-Variable -Name Application
-        New-Variable -Name Config
-        New-Variable -Name HTMLFile
-        New-Variable -Name Pester_Variables
-        New-Variable -Name Test_Results
-        #############################
-        Write-Verbose -Message "Need to create a temp file to store our HTML"
-        $HTMLFile = ($env:TEMP + "Body.html")
-        if ( Test-Path $HTMLFile) {
-            Remove-Item $HTMLFile
-        }#if
+	[cmdletbinding()]
+	param(
+		[parameter(Mandatory = $true)]
+		[ValidateNotNullOrEmpty()]
+		[ValidateScript( {
+				if ( $_ -like "*.json") {
+					if ( Test-Path $_) {
+						$True
+					}#Test-Path True
+					else {
+						Throw "Unable to locate $_"
+					}#Test-Path false
+				}#if match json true
+				else {
+					Throw "$_ must be a valid psd1 or JSON file"
+				}#if match json false
+			}
+		)]
+		[string]$ConfigFile,
+		[parameter(	Mandatory = $false	)]
+		[ValidateSet("Simple", "Comprehensive")]
+		[string]$TestType = "Simple",
+		[parameter(Mandatory = $False)]
+		[ValidateNotNullOrEmpty()]
+		[string[]]$Tags,
+		[parameter(
+			Mandatory = $true,
+			ValueFromPipeline = $true
+		)]
+		[ValidateNotNullOrEmpty()]
+		[ValidateScript(
+			{	
+				Import-Module $_ 
+				if ( (Get-Module $_).Count -ge 1) {
+					$true
+				}
+				else {
+					throw "$_ is not a valid application module"
+				}
+			}
+		)]
+		[string]$Application,
+		[Parameter(Mandatory = $True)]
+		[string]$SmtpServer,
+		[Parameter(Mandatory = $True)]
+		[string]$From,
+		[parameter(Mandatory = $true)]
+		[ValidateNotNullOrEmpty()]
+		[string[]]$To
+	)
+	begin {
+		New-Variable -Name Application
+		New-Variable -Name Config
+		New-Variable -Name HTMLFile
+		New-Variable -Name Pester_Variables
+		New-Variable -Name Test_Results
+		#############################
+		Write-Verbose -Message "Need to create a temp file to store our HTML"
+		$HTMLFile = ($env:TEMP + "Body.html")
+		if ( Test-Path $HTMLFile) {
+			Remove-Item $HTMLFile
+		}#if
 		
-        $Pester_Variables = @{
-            Script       = $TestsPath
-            OutputFile   = ($env:TEMP + "\TestResults.xml")
-            OutputFormat = "NUnitxml"
-        }#Pester_Variables
-    }
-    process {
-        Write-Verbose -Message ("Get Module " + $Application + " path of Module and append appropriate diagnostics folder information")
-        Write-Verbose -Message ("ModuleBase is : " + (Get-Module $Application).ModuleBase)
-        $TestsPath = ((Get-Module $Application).ModuleBase + "\Diagnostics\" + $TestType)
-        #############################
-        Write-Verbose -Message ("Import JSON file:  " + $ConfigFile )
-        #ConvertPSObjectToHashtable is defined at the top of this file
-        #we need the function to do appropriate conversion of multiple nested levels
-        #(ConvertFrom-Json $json).psobject.properties | ForEach-Object { $config[$_.Name] = $_.Value }
-        #only gives one level deep of a hashtable
+		$Pester_Variables = @{
+			Script       = $TestsPath
+			OutputFile   = ($env:TEMP + "\TestResults.xml")
+			OutputFormat = "NUnitxml"
+		}#Pester_Variables
+	}
+	process {
+		Write-Verbose -Message ("Get Module " + $Application + " path of Module and append appropriate diagnostics folder information")
+		Write-Verbose -Message ("ModuleBase is : " + (Get-Module $Application).ModuleBase)
+		$TestsPath = ((Get-Module $Application).ModuleBase + "\Diagnostics\" + $TestType)
+		#############################
+		Write-Verbose -Message ("Import JSON file:  " + $ConfigFile )
+		#ConvertPSObjectToHashtable is defined at the top of this file
+		#we need the function to do appropriate conversion of multiple nested levels
+		#(ConvertFrom-Json $json).psobject.properties | ForEach-Object { $config[$_.Name] = $_.Value }
+		#only gives one level deep of a hashtable
 		
-        #This probably needs to be reviewed as it assumes the integration tests will utilize $Config
-        $Config = Get-Content -Path $ConfigFile | ConvertFrom-Json | ConvertTo-PLXSHashtable
+		#This probably needs to be reviewed as it assumes the integration tests will utilize $Config
+		$Config = Get-Content -Path $ConfigFile | ConvertFrom-Json | ConvertTo-PLXSHashtable
 
-        <#
-        Honestly, I have had worse ideas than this, but not many
-        and I can't remember what they were.
-        The reportunit.exe provides some very nice output for "graphical" view of the results.
-        #>
-        $ReportUnit = (Get-Module PLXS_RedPen).ModuleBase + "\Private\ReportUnit.exe"
-        Write-Verbose "Scripts directory is $TestsPath"
-        Write-Verbose "Checking configuration first"
-        $Test_Results = Invoke-Pester -Tags "Config" -PassThru @Pester_Variables
+		<#
+		Honestly, I have had worse ideas than this, but not many
+		and I can't remember what they were.
+		The reportunit.exe provides some very nice output for "graphical" view of the results.
+		#>
+		$ReportUnit = (Get-Module PLXS_RedPen).ModuleBase + "\Private\ReportUnit.exe"
+		Write-Verbose "Scripts directory is $TestsPath"
+		Write-Verbose "Checking configuration first"
+		$Test_Results = Invoke-Pester -Tags "Config" -PassThru @Pester_Variables
 		
-        If ( $Test_Results.FailedCount -gt 0) {		
-            Write-Error "Invalid Configuration, please correct config definition failures."
-        }#if
-        else {
-            <#
+		If ( $Test_Results.FailedCount -gt 0) {		
+			Write-Error "Invalid Configuration, please correct config definition failures."
+		}#if
+		else {
+			<#
 				If the configuration tests pass, run the rest of the integration tests.
 			#>
-            $Test_Results = Invoke-Pester -Tag $Tags -PassThru -ExcludeTag "Config" @Pester_Variables
-        }#else
-        <#
+			$Test_Results = Invoke-Pester -Tag $Tags -PassThru -ExcludeTag "Config" @Pester_Variables
+		}#else
+		<#
 			The -PassThru parameter allows for the test results to be passed back and to formulate an e-mail.
 			Because we get all test reults back, we need to check just the failed count.
 		#>
-        . $ReportUnit $env:TEMP\TestResults.xml $env:TEMP\TestResults.html
-        $Subject = ($Application + " : " + $ConfigFile + " Configuration failures identified")
-        $Body = Get-Content -Path $env:TEMP\TestResults.html -Raw
-        Send-MailMessage -From $From -To $To -SmtpServer $SmtpServer -Subject $Subject -BodyAsHtml $Body
-    }#Process
-    end {
-        Remove-variable -Name Application, Config, HTMLFile, Pester_Variables, Test_Results
-    }#End
+		. $ReportUnit $env:TEMP\TestResults.xml $env:TEMP\TestResults.html
+		$Subject = ($Application + " : " + $ConfigFile + " Configuration failures identified")
+		$Body = Get-Content -Path $env:TEMP\TestResults.html -Raw
+		Send-MailMessage -From $From -To $To -SmtpServer $SmtpServer -Subject $Subject -BodyAsHtml $Body
+	}#Process
+	end {
+		Remove-variable -Name Application, Config, HTMLFile, Pester_Variables, Test_Results
+	}#End
 }#Function
--- Module/Functions/New-ConfigFile.psm1
+++ Module/Functions/New-ConfigFile.psm1
@@ -1,39 +1,38 @@
 Function New-ConfigFile {
-    [cmdletbinding()]
-    param(
-        [parameter( Mandatory = $true)]
-        [ValidateNotNullOrEmpty()]
-        [ValidateScript(
-            {    
-                Import-Module $_ 
-                if ( (Get-Module $_).Count -ge 1) {
-                    $true
-                }
-                else {
-                    throw "$_ is not a valid application"
-                }
-            }
-        )]
-        [string]$Application,
-        [Parameter(Mandatory = $true)]
-        [string]$Destination
-    )
-    begin {
-        New-Variable -Name ModuleBase -Value (Get-Module $Application).ModuleBase
-    }
-    process {
-        Write-Verbose -Message ("Get Module " + $Application + " path of Module and append appropriate template folder information")
-        Write-Verbose -Message ("ModuleBase is : " + $ModuleBase)
-        $Template = ($ModuleBase + "\Template\")
-        if ( -not (Test-Path $Template)) {
-            throw (Write-Error -Message "Application does not contain applicable templates.")
-        }
-        elseif ( -not (Test-Path "$Template\PlasterManifest.xml")) {
-            throw (Write-Error -Message "Application does not contain applicable templates.")
-        }		
-        Invoke-Plaster -TemplatePath $Template -DestinationPath $Destination -NoLogo
-    }#Process
-    end {
-        Remove-variable -Name ModuleBase, Template
-    }#End
+	[cmdletbinding()]
+	param(
+		[parameter( Mandatory = $true)]
+		[ValidateNotNullOrEmpty()]
+		[ValidateScript({	
+				Import-Module $_ 
+				if ( (Get-Module $_).Count -ge 1) {
+					$true
+				}
+				else {
+					throw "$_ is not a valid application"
+				}
+			}
+		)]
+		[string]$Application,
+		[Parameter(Mandatory = $true)]
+		[string]$Destination
+	)
+	begin {
+		New-Variable -Name ModuleBase -Value (Get-Module $Application).ModuleBase
+	}
+	process {
+		Write-Verbose -Message ("Get Module " + $Application + " path of Module and append appropriate template folder information")
+		Write-Verbose -Message ("ModuleBase is : " + $ModuleBase)
+		$Template = ($ModuleBase + "\Template\")
+		if ( -not (Test-Path $Template)) {
+			throw (Write-Error -Message "Application does not contain applicable templates.")
+		}
+		elseif ( -not (Test-Path "$Template\PlasterManifest.xml")) {
+			throw (Write-Error -Message "Application does not contain applicable templates.")
+		}		
+		Invoke-Plaster -TemplatePath $Template -DestinationPath $Destination -NoLogo
+	}#Process
+	end {
+		Remove-variable -Name ModuleBase, Template
+	}#End
 }#Function
